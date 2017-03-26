cb5e4ff64a8e229ac7e81f9351d59d27a6571640
Theodore Messinezis <theo@theomessin.com>
2017-03-26 14:08:56 +0100

    Present

--- app/Http/Controllers/QrevController.php
+++ app/Http/Controllers/QrevController.php
@@ -12,7 +12,7 @@ class QrevController extends Controller
      * @param null $ident
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function landing($ident= null)
+    public function landing($ident = null)
     {
         $session = str_random(16);
         $key = str_random(16);
@@ -25,4 +25,31 @@ class QrevController extends Controller
 
         return view('landing')->with(compact('session', 'key'))->with('share', $ident !== null);
     }
+
+    /**
+     * Demo page QR code
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function demo()
+    {
+        $session = "0B8qt43gkyP32l6u";
+        $key = "P3c788gcEa8l480H";
+
+        return view('landing')->with(compact('session', 'key'))->with('share', false);
+    }
+
+    /**
+     * Demo page QR code
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function demoShare()
+    {
+        $session = "0B8qt43gkyP32l6u";
+        $key = "P3c788gcEa8l480H";
+
+        return view('landing')->with(compact('session', 'key'))->with('share', true);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,5 @@
 <?php
 
+Route::get('/hc', 'QrevController@demo');
+Route::get('/hackcity17', 'QrevController@demoShare');
 Route::get('/{ident?}', 'QrevController@landing');
